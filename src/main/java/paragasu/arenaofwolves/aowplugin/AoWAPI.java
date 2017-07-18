10e506de662e73eb557446bc4f8a83f5e157fb8c
Paragasu <paragasu1232@gmail.com>
2017-07-19 03:48:10 +0900

    wolfType名を返すメソッドの削除（WolfTypeクラスに移動）

--- src/main/java/paragasu/arenaofwolves/aowplugin/AoWAPI.java
+++ src/main/java/paragasu/arenaofwolves/aowplugin/AoWAPI.java
@@ -1,12 +1,4 @@
 package paragasu.arenaofwolves.aowplugin;
 
 public class AoWAPI {
-	public static WolfType getWolfTypeFromString(String wolfTypeName) {
-		if(wolfTypeName.equalsIgnoreCase("Wyatt")) {
-			return WolfType.Wyatt;
-		}
-		else{
-			return null;
-		}
-	}
 }
