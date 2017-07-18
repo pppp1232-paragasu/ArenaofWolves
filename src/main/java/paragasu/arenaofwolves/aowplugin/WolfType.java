3840e3695f0bbd5d524a5012962dc04e5f234adc
Paragasu <paragasu1232@gmail.com>
2017-07-19 03:48:29 +0900

    [add]getTypeNameメソッドの追加

--- src/main/java/paragasu/arenaofwolves/aowplugin/WolfType.java
+++ src/main/java/paragasu/arenaofwolves/aowplugin/WolfType.java
@@ -3,5 +3,11 @@ package paragasu.arenaofwolves.aowplugin;
 enum WolfType {
 	NORMAL,
 	TEST,
-	Wyatt,
+	WYATT;
+
+	public  String getTypeName() {
+		String name = this.name();
+		name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+		return name;
+	}
 }
