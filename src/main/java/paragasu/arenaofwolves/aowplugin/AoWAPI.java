2ed84c8f38e27fc6378cec3321b84ccb15115e2c
Paragasu <paragasu1232@gmail.com>
2017-07-19 16:27:41 +0900

    [add]sendGameMessageメソッドを追加

--- src/main/java/paragasu/arenaofwolves/aowplugin/AoWAPI.java
+++ src/main/java/paragasu/arenaofwolves/aowplugin/AoWAPI.java
@@ -1,4 +1,11 @@
 package paragasu.arenaofwolves.aowplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 public class AoWAPI {
+
+	public static void sendGameMessage(Player player, String message) {
+		player.sendMessage(ChatColor.BOLD +"[" + ChatColor.YELLOW +"AoW" + ChatColor.WHITE + "]" + ChatColor.RESET + message);
+	}
 }
