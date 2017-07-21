48021a80ad737b50383ba935596068dc7b7eb769
Paragasu <paragasu1232@gmail.com>
2017-07-21 15:01:03 +0900

    [fix]正しくステータスが設定されないバグを修正

--- src/main/java/paragasu/arenaofwolves/aowplugin/fightingwolf/Wyatt.java
+++ src/main/java/paragasu/arenaofwolves/aowplugin/fightingwolf/Wyatt.java
@@ -7,17 +7,25 @@ import org.bukkit.entity.Wolf;
 
 public class Wyatt extends FightingWolf {
 
-	protected int hitPoints = 200;
-	protected int attack = 10;
-	protected int defence = 20;
-	protected int critChance = 10;
-	protected int critDamage = 5;
-	protected int accuracy = 100;
-	protected int evasion = 0;
-	protected int magicPower = 0;
+//	protected int hitPoints = 200;
+//	protected int attack = 10;
+//	protected int defence = 20;
+//	protected int critChance = 10;
+//	protected int critDamage = 5;
+//	protected int accuracy = 100;
+//	protected int evasion = 0;
+//	protected int magicPower = 0;
 
 	Wyatt(Player owner, Wolf wolf, ArrayList<Integer> allocatedPoints) {
 		super(owner, wolf, allocatedPoints);
+		this.hitPoints += 200;
+		this.attack += 10;
+		this.defence += 20;
+		this.critChance += 10;
+		this.critDamage += 5;
+		this.accuracy += 100;
+		this.evasion += 0;
+		this.magicPower += 0;
 	}
 
 	@Override
