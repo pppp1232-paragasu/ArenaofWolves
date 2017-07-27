e96cc62bd9ccc6e0f88d768458afbf5274faea42
Paragasu <paragasu1232@gmail.com>
2017-07-27 10:19:02 +0900

    [add]onSignClickedメソッドの追加

--- src/main/java/paragasu/arenaofwolves/aowplugin/EventListener.java
+++ src/main/java/paragasu/arenaofwolves/aowplugin/EventListener.java
@@ -1,11 +1,17 @@
 package paragasu.arenaofwolves.aowplugin;
 
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 import paragasu.arenaofwolves.aowplugin.fightingwolf.FightingWolf;
 
@@ -32,6 +38,24 @@ public class EventListener implements Listener {
 			FightingWolf fightingWolf = FightingWolf.getFightingWolf(wolf);
 			if(fightingWolf != null) {
 				FightingWolf.fightingWolfSet.remove(fightingWolf);
+			}
+		}
+	}
+
+	@EventHandler
+	public void onSignClicked(PlayerInteractEvent e) {
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			Material material = e.getClickedBlock().getType();
+			if(material == Material.SIGN || material == Material.SIGN_POST || material == Material.WALL_SIGN) {
+				Player player = e.getPlayer();
+				Sign sign = (Sign) e.getClickedBlock().getState();
+				if(sign.getLine(1).indexOf("ステージ1") != -1) {
+			        if (e.getHand() == EquipmentSlot.OFF_HAND) {
+			            return;
+			        }
+					DebugStage stage = DebugStage.getInstance();
+					stage.matching(player);
+				}
 			}
 		}
 	}
